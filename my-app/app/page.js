91e5300782d9298380b7255badd11453221ff02b
Giovanni Barosco <g.barosco@donboscosandona.it>
2025-06-09 13:32:51 +0200

    a

---
+++ my-app/app/page.js
@@ -0,0 +1,23 @@
+import Navbar from "./components/Navbar"
+
+
+
+export default function Home() {
+  return (
+    <>
+
+<Navbar
+        professionalEmail={professionalEmail}
+        professionalPhone={professionalPhone}
+        excursionEmail={excursionEmail}
+        excursionPhone={excursionPhone}
+        logoSrc={logoSrc}
+        menuItems={menuItems}
+      />
+
+
+</>
+  );
+}
+
+
